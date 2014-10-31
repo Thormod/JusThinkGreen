67cd804275f219587ab86f16f9b9cdd78707d226
Daniel Arango <skap.mks@gmail.com>
2014-10-31 17:46:06 -0500

    Se mejora el view post.create

--- app/views/post/create.blade.php
+++ app/views/post/create.blade.php
@@ -4,43 +4,41 @@
 
 {{ HTML::script('packages/ckeditor/ckeditor/ckeditor.js') }}
 
-{{ Form::open(['route' => 'post.store']) }}
-
-	<div style="text-align: center">
-	<br>
-	<br>
-	<title>Crear Publicacion</title>
-	<h1 style="text-align: center"> Crear Publicacion </h1>
-	<br>
-		{{Form::Label('labTitle', 'Titulo: ')}}
-	</br>
-		{{Form::Text('title')}}
-		{{$errors->first('title', 'Titulo requerido')}}
-	</br>
-	</br>
-	</div>
-
-	<div  style="text-align: center">
-	<br>
-		{{Form::Label('labContent', 'Contenido: ')}}
-	</br>
-
-		{{Form::Textarea('content_text')}}
-		<script>
-
-			$content = CKEDITOR.replace( 'content_text', {
-				height: 350,
-			});
-
-			$editor_data = CKEDITOR.instances.content_text.getData();
-		</script>	
-		{{$errors->first('content_text', 'Contenido requerido')}}	
-	</div>
+<br><br><br>
+<div class="panel panel-default" style="text-align: center; width:auto; max-width:1400px; margin:auto;">
 	
-	<div style="text-align: center">
-		{{Form::submit('Crear publicacion')}}
+	<div class="panel-heading">
+		Crear Publicacion
 	</div>
 
-{{ Form::close() }}
+	<div class="panel-body">
+		{{ Form::open(['route' => 'post.store']) }}
 
+		<div style="text-align: center">
+			{{Form::Label('labTitle', 'Titulo: ')}}
+			{{Form::Text('title')}}
+			{{$errors->first('title', 'Titulo requerido')}}
+		</div>
+		<div>
+			{{Form::Textarea('content_text')}}
+			<script>
+
+				$content = CKEDITOR.replace( 'content_text', {
+					height: 350,
+				});
+
+				$editor_data = CKEDITOR.instances.content_text.getData();
+			</script>	
+			{{$errors->first('content_text', 'Contenido requerido')}}	
+		</div>
+	</div>
+
+	<div class="panel-footer">
+	<div style="text-align: center">
+			{{Form::submit('Crear publicacion')}}
+		</div>
+	</div>
+
+	{{ Form::close() }}
+</div>
 @stop
