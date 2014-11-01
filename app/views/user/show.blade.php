70f88671d0b5d830f3739dd088b3c9170a554a7c
Daniel Arango <skap.mks@gmail.com>
2014-10-31 20:31:53 -0500

    Se mejoro el profile de usuario

--- app/views/user/show.blade.php
+++ app/views/user/show.blade.php
@@ -1,3 +1,74 @@
 @extends('base')
 <br><br><br>
 
+
+<div class="container-fluid" style="background-color: white; margin:50px">
+	<div class="row; row; col-md-2">
+	  <ul class="list-group" style="max-width: 350px;">
+		  <li class="list-group-item">Cras fsdfsdfsdto odio</li>
+		  <li class=	"list-group-item">Dapibus ac facilisis in</li>
+		  <li class="list-group-item">Morbi leo risus</li>
+		  <li class="list-group-item">Porta ac consectetur ac</li>
+		  <li class="list-group-item">Vestibulum at eros</li>
+		</ul>
+
+		<div class>
+		  <div class="panel panel-default">
+		  	<div class="panel-heading">
+		  		Logros
+		  	</div>
+		  	<div class="panel-body">
+		  		Iconos de Logros
+		  	</div>
+		  </div>
+		</div>
+		
+	</div>
+	
+	<div class="row; col-md-8" style="text-align: center">
+		Mejores Publicaciones
+	</div>
+
+	<div class="row; col-md-8">
+	  <div class="panel panel-default">
+	  	<div class="panel-heading">
+	  		test
+	  	</div>
+	  	<div class="panel-body">
+	  		body
+	  	</div>
+	  </div>
+	</div>
+
+	<div class="row; col-md-8">
+	  <div class="panel panel-default">
+	  	<div class="panel-heading">
+	  		test
+	  	</div>
+	  	<div class="panel-body">
+	  		body
+	  	</div>
+	  </div>
+	</div>
+	<div class="row; col-md-8">
+	  <div class="panel panel-default">
+	  	<div class="panel-heading">
+	  		test
+	  	</div>
+	  	<div class="panel-body">
+	  		body
+	  	</div>
+	  </div>
+	</div>
+	<div class="row; col-md-8">
+	  <div class="panel panel-default">
+	  	<div class="panel-heading">
+	  		test
+	  	</div>
+	  	<div class="panel-body">
+	  		body
+	  	</div>
+	  </div>
+	</div>
+
+</div>
